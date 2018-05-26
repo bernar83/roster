2a0d179c0f317549956637878e0c3b0d881cd95a
Adrian Bernardo <nabernardo93@gmail.com>
2018-05-25 17:27:13 -0700

    Declare Roster class

--- roster/roster.h
+++ roster/roster.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "student.h"
+
+class Roster
+{
+public:
+	~Roster();
+	void Add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, Degree program);
+	void Remove(string studentID);
+	void PrintAll();
+	void PrintDaysInCourse(string studentID);
+	void PrintInvalidEmails();
+	void PrintByDegreeProgram();
+
+private:
+	Student * ClassRosterArray[5] = { nullptr, nullptr, nullptr, nullptr, nullptr };
+
+};
